8248ea3c2eec7aa5e94180657ba4c444e1593f89
Leon <huidong.liang@eng.ox.ac.uk>
2026-05-03 00:22:42 +0100

    some updates

--- run.sh
+++ run.sh
@@ -27,15 +27,15 @@ run_models(){
 # Unomment to execute in parallel
 DATASETS=(
     #paris
-    #shanghai
-    la
+    shanghai
+    #la
     #london
 )
 
 # Given a machine with 8 GPUs and 80 cores,
 # the tasks can be executed in parallel
 for data in ${DATASETS[@]}; do
-    run_models $data gcn 0 0 10 &
+    run_models $data gat 0 0 10 &
     #run_models $data sage 1 10 10 &
     #run_models $data gat 2 20 10 &
     #run_models $data gcnii 3 30 10 &
